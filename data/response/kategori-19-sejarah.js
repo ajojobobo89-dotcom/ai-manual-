b6a5fcfefca80d69d538226bcffe907c636106ef
ajojobobo89-dotcom <ajojobobo89@gmail.com>
2026-03-07 06:27:43 +0700

    Create kategori-19-sejarah.js

---
+++ data/response/kategori-19-sejarah.js
@@ -0,0 +1,20 @@
+// Kategori: Sejarah (40+ respons)
+
+const SEJARAH_RESPONSES = [
+    {
+        id: "sejarah-001",
+        keywords: ["proklamasi", "kemerdekaan indonesia", "17 agustus"],
+        response: "**Proklamasi Kemerdekaan Indonesia**\n\n📅 **Tanggal:** 17 Agustus 1945\n📍 **Tempat:** Jalan Pegangsaan Timur 56, Jakarta\n👤 **Pembaca:** Soekarno-Hatta\n\n**Teks Proklamasi:**\n\"Kami bangsa Indonesia dengan ini menjatakan kemerdekaan Indonesia. Hal-hal jang mengenai pemindahan kekoeasaan d.l.l., diselenggarakan dengan tjara saksama dan dalam tempo jang sesingkat-singkatnja. Djakarta, hari 17 boelan 8 tahoen 05. Atas nama bangsa Indonesia. Soekarno/Hatta.\"\n\n**Peristiwa Penting:**\n• Rengasdengklok (16 Agustus)\n• Penyusunan teks di rumah Laksamana Maeda\n• Pembacaan proklamasi pukul 10.00\n• Pengibaran bendera merah putih\n\n**Makna:**\nMenandai berdirinya Negara Kesatuan Republik Indonesia.",
+        category: "sejarah"
+    },
+    {
+        id: "sejarah-002",
+        keywords: ["soekarno", "bung karno", "presiden pertama"],
+        response: "**Ir. Soekarno** - Presiden Pertama RI\n\n📅 **Lahir:** 6 Juni 1901, Surabaya\n📅 **Wafat:** 21 Juni 1970, Jakarta\n\n**Peran Penting:**\n• Proklamator kemerdekaan\n• Presiden pertama RI (1945-1967)\n• Penggali Pancasila\n• Orator ulung\n• Arsitek (merancang Monas, GBK)\n\n**Gelar:**\n• Putra Sang Fajar\n• Bung Karno\n• Penyambung Lidah Rakyat\n\n**Kutipan Terkenal:**\n\"Jangan sekali-kali melupakan sejarah\" (Jasmerah)\n\"Beri aku 1000 orang tua, niscaya akan kucabut Semeru dari akarnya. Beri aku 10 pemuda, niscaya akan kuguncangkan dunia.\"\n\n**Peninggalan:**\n• Pancasila sebagai dasar negara\n• Semangat anti kolonialisme\n• Pidato-pidato inspiratif",
+        category: "sejarah"
+    }
+];
+
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = SEJARAH_RESPONSES;
+}
